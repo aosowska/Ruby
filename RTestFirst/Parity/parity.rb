d55db8813a02afd54fdbe03fee48b12e33ebcd32
aosowska <alek.osowska@gmail.com>
2016-11-12 22:44:38 +0100

    Add files via upload

---
+++ RTestFirst/Parity/parity.rb
@@ -0,0 +1,11 @@
+#Parity
+class Parity
+  def isEven(a)
+    if((a%2) == 0)
+      return true;
+    else
+      return false;
+    end
+  end
+end
+
---
+++ RTestFirst/Sign/sign.rb
@@ -0,0 +1,10 @@
+# Sign
+class Sign
+  def floor(x)
+    if y > 0
+      return true;
+    elsif y <= 0
+      return false;
+    end
+  end
+end
